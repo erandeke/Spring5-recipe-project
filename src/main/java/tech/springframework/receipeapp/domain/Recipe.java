9f0c794a72eeb14f45678387206c2e502aac410d
erandeke <erande.kedar@gmail.com>
2018-12-24 16:51:07 +0530

    Added Releationship model between Receipe and Ingredients

--- src/main/java/tech/springframework/receipeapp/domain/Recipe.java
+++ src/main/java/tech/springframework/receipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package tech.springframework.receipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,11 +18,23 @@ public class Recipe {
     private String url;
     private String direction;
 
-    @OneToOne(cascade= CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredients> ingredients;
+
+
     private byte[] image;
+
+    public Set<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public Long getId() {
         return id;
